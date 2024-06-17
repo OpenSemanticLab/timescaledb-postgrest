2527272efb01e41bb240645111815b1178181ef8
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2024-06-17 15:22:26 +0000

    add synthetic data gen function at backend

--- docker-entrypoint/4_init_schema4.sql
+++ docker-entrypoint/4_init_schema4.sql
@@ -50,3 +50,66 @@ JOIN api.channeldata cd
 ON c.osw_channel = cd.osw_channel;
 GRANT SELECT on api.view_tooldata to api_anon;
 GRANT ALL on api.view_tooldata to api_user;
+
+-- Create synthetic data for query testing
+-- Function to generate synthetic data into channeldata, input is channel id and number of data points
+DROP FUNCTION IF EXISTS api.gen_data;
+CREATE OR REPLACE FUNCTION api.gen_data(channel CHAR(35), datapoints INT, days_offset INT DEFAULT 0, tool CHAR(35) DEFAULT NULL)
+-- Return message if channel and tool do not exist
+RETURNS TEXT AS $$
+DECLARE
+  create_msg TEXT;
+  insert_msg TEXT;
+  _tool CHAR(35);
+
+BEGIN
+  create_msg := '';
+  insert_msg := '';
+  -- Check if tool is provided, if not use tool from channel
+  IF tool IS NOT NULL THEN
+    _tool := tool;
+  ELSE
+    _tool := (SELECT osw_tool FROM api.channels WHERE osw_channel = channel);
+  END IF;
+
+  -- Raise Error if tool is not provided and not found in channel
+  IF _tool IS NULL THEN
+    RAISE EXCEPTION 'The provided channel is not associated with a tool, either use an existing channel or provide a tool id to be created!';
+  END IF;
+
+  -- Raise Error if channel exists and tool does not
+  IF EXISTS (SELECT * FROM api.channels WHERE osw_channel = channel AND osw_tool <> _tool) THEN
+    RAISE EXCEPTION 'Channel exists but tool does not match!';
+  END IF;
+
+  -- Create channel if not exists but tool exists
+  IF EXISTS (SELECT 1 FROM api.channels WHERE osw_channel <> channel AND osw_tool = _tool) THEN
+    -- Case 1 Channel does not exist at all and tool exists -> create channel with tool
+    IF NOT EXISTS (SELECT 1 FROM api.channels WHERE osw_channel = channel) THEN
+      INSERT INTO api.channels (osw_channel, osw_tool) VALUES (channel, _tool);
+      -- message that channel and tool were created
+      RAISE NOTICE 'Channel for exisiting Tool created successfully!'; 
+      create_msg := 'Channel for existing Tool and ';
+    END IF;
+  END IF;
+
+  -- Create channel and tool if both do not exist
+  IF NOT EXISTS (SELECT 1 FROM api.channels WHERE osw_channel = channel AND osw_tool = _tool) THEN
+    INSERT INTO api.channels (osw_channel, osw_tool) VALUES (channel, _tool);
+    -- message that channel and tool were created
+    RAISE NOTICE 'Channel and Tool created successfully!'; 
+    create_msg := 'Channel, Tool and ';
+  END IF;
+  
+  INSERT INTO api.channeldata(osw_channel, ts, data)
+  SELECT
+    channel,
+    '2024-01-01T00:00:00.000000+00'::TIMESTAMPTZ + (days_offset * interval '1 day') + (generate_series(1, datapoints) ) * interval '1 microseconds',
+    ('{"value":'||(random()*100)::real||'}')::json;
+  -- message that data was created
+  RAISE NOTICE 'Data generated successfully!';
+  insert_msg := 'Data generated successfully!';
+
+  RETURN create_msg || insert_msg;
+END;
+$$ LANGUAGE plpgsql;
